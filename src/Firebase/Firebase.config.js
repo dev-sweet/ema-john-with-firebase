443714d36f55694232915feeca0958275cbe4cbe
dev-sweet <sweetali0520@gmail.com>
2021-10-11 22:09:32 +0600

    my commit start

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQJZ_LMYjyQP-OjV6vALrHfv2CQEo5y5w",
+  authDomain: "ema-john-with-firebase-a736a.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a736a",
+  storageBucket: "ema-john-with-firebase-a736a.appspot.com",
+  messagingSenderId: "877157124850",
+  appId: "1:877157124850:web:9e5bc448d9fd0a08fede6b",
+};
+export default firebaseConfig;
---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,6 @@
+import firebaseConfig from "./Firebase.config";
+
+const initializationAuthentication = () => {
+  initializeApp(firebaseConfig);
+};
+export default initializationAuthentication();
